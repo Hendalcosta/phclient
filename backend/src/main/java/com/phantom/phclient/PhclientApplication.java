dc4464c12dda7583437f9c40e42cad4ff37a3edb
Henderson <hendalcosta@gmail.com>
2022-12-21 16:24:51 -0300

    Project created

---
+++ backend/src/main/java/com/phantom/phclient/PhclientApplication.java
@@ -0,0 +1,13 @@
+package com.phantom.phclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhclientApplication.class, args);
+	}
+
+}
